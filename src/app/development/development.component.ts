e7ec20e69994e536112dc5c04a224bc2c38ffa23
William Watlington <fictionfreeway@gmail.com>
2025-03-08 21:11:58 -0500

    Add development component with initial HTML, CSS, and TypeScript files

---
+++ src/app/development/development.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'development',
+  standalone: true,
+  templateUrl: './development.component.html',
+  styleUrls: ['./development.component.css']
+})
+export class MyNewComponent { }
