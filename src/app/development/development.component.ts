3193529844c8db9315eb58cd56673058aea62771
William Watlington <fictionfreeway@gmail.com>
2025-03-09 14:52:00 -0400

    Update development component to use inline styles and HTML template, remove unused CSS

--- src/app/development/development.component.ts
+++ src/app/development/development.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
 
-import template from './development.component.css?raw';
-import styles from './development.component.css?raw';
+import template from './development.component.html?raw';
+import styles from './development.component.css?inline';
 
 @Component({
   selector: 'app-development',
   standalone: true,
+  imports: [CommonModule, RouterLink, RouterModule],
   template: template || '',
   styles: [styles || '']
 })
