00fc0a978d641650a6cc73d5af4a02eb92f55fa6
William Watlington <fictionfreeway@gmail.com>
2025-03-14 16:05:29 -0400

    Add BRRL showcase content and styles to design component

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -26,7 +26,7 @@ export class DesignComponent implements OnInit {
     const templatePaths = [
       '/assets/showcases/Design.html',
       '/assets/showcases/PES_40th_Anniversary_Campaign.html',
-      '/assets/showcases/template3.html',
+      '/assets/showcases/BRRL_Design.html',
     ];
 
     templatePaths.forEach((path) => {
