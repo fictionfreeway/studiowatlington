67c99d2f15be046bdf252bc08439edec37d03953
William Watlington <fictionfreeway@gmail.com>
2025-03-31 12:09:47 -0400

    Update social design showcase video and adjust paragraph styling; correct file path for BRRL design template

--- src/app/design/design.component.ts
+++ src/app/design/design.component.ts
@@ -25,7 +25,7 @@ export class DesignComponent implements OnInit {
     const templatePaths = [
       '/assets/showcases/Design.html',
       /* '/assets/showcases/PES_40th_Anniversary_Campaign.html', */
-      '/assets/showcases/BRRL_Design.html',
+      '/assets/showcases/brrl_design.html',
       '/assets/showcases/social_design.html'
     ];
   
