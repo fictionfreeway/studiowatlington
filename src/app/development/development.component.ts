9943e405d24003fda623ca9cd98fb6c652f51ca8
William Watlington <fictionfreeway@gmail.com>
2025-05-18 05:14:03 -0400

    Add responsive design and animations for development showcase
    - Introduced a new background window element for the showcase.
    - Adjusted CSS styles for desktop and mobile screenshots to improve layout.
    - Implemented media queries for portrait and landscape orientations to enhance responsiveness.
    - Updated animations for images based on device orientation, ensuring a smooth user experience.
    - Refactored animation logic to differentiate between landscape and portrait modes.

--- src/app/development/development.component.ts
+++ src/app/development/development.component.ts
@@ -65,14 +65,15 @@ export class DevelopmentComponent implements AfterViewInit {
     .add('#projects-header', {
       opacity: 0,
       scale: 2.2,
+      left: '40%',
       duration: 150
     }, 1)
     // whiteboard starting position
     .add('#whiteboard-container-desktop', {
-      width: '75vw',
+      width: '60vw',
       height: '75vh',
-      left: '10%',
-      top: '10%',
+      left: '5%',
+      top: '15%',
       duration: 0
     }, 0)
     // whiteboard expands to cover the viewport
@@ -96,7 +97,19 @@ export class DevelopmentComponent implements AfterViewInit {
       bottom: '-60vh',
       scale: 0.8,
       duration: 750
-    }, 250)
+    }, 1)
+    // window starting position
+    .add('#background-window', {
+      right: 0,
+      top: 0,
+      width: '35vw'
+    }, 0)
+    // window scales up and moves to the right to match the whiteboard
+    .add('#background-window', {
+      right: '-250%',
+      scale: 4,
+      duration: 250,
+    }, 1)
 
     // draw the opening 'projects' header
     animate(svg.createDrawable('.projects-header-path'), {
@@ -148,126 +161,226 @@ export class DevelopmentComponent implements AfterViewInit {
       })
     })
 
-    const tl = createTimeline({
-      defaults: {},
-    })
-    // menu img starting position
-    .add('#brrl-menu-img', {
-      right: [
-        {to: '-25%'}
-      ],
-      rotate: [
-        {to: '-40deg'}
-      ],
-      top: [
-        {to: '-25rem'}
-      ],
-      duration: 0
-    }, 0)
-    // menu img slides in from right
-    .add('#brrl-menu-img', {
-      right: [
-        {to: '19%' }
-      ],
-      rotate: [
-        {to:'8deg'}
-      ],
-      top: [
-        {to: '-10rem'}
-      ],
-      duration: 500
-    }, 250)
-    // menu img flies away to the right
-    .add('#brrl-menu-img', {
-      right: [
-        {to: '-25%'}
-      ],
-      rotate: [
-        {to: '-40deg'}
-      ],
-      top: [
-        {to: '-25rem'}
-      ],
-      duration: 150,
-      ease: 'inBack'
-    }, 750)
-    // text fades in
-    .add('#brrl-text', {
-      opacity: [0, 1],
-      translateY: ['-5rem', '0rem'],
-      duration: 250
-    }, 250)
-    // mobile img initial position
-    .add('#brrl-mobile-img', {
-      right: '-95%',
-      top: '50rem',
-      rotate: '20deg',
-      duration: 0
-    }, 0)
-    // mobile img flies in from the right
-    .add('#brrl-mobile-img', {
-      right: '25%',
-      top: '-15rem',
-      rotate: '-5deg',
-      duration: 500,
-      ease: 'outBack'
-    }, 50)
-    // mobile image moves down and rotates slightly to accompany menu-img
-    .add('#brrl-mobile-img', {
-      right: '22%',
-      rotate: '-2deg',
-      top: '-12rem',
-      duration: 250,
-      ease: 'outBack'
-    }, 250)
-    // mobile img flies away to the right
-    .add('#brrl-mobile-img', {
-      right: '55%',
-      top: '-125rem',
-      rotate: '20deg',
-      duration: 250,
-      ease: 'inBack'
-    }, 750)
-    // website img starting position
-    .add('#brrl-website-img', {
-      left: '-95%',
-      top: '30rem',
-      rotate: '20deg',
-      duration: 0
-    }, 0)
-    // website img slides in from the left
-    .add('#brrl-website-img', {
-      left: '15%',
-      top: '2rem',
-      rotate: '0deg',
-      duration: 250,
-      ease: 'inOutBack'
-    }, 1)
-    // website img move down and rotate slightly to accompany the text
-    .add ('#brrl-website-img', {
-      top: '14rem',
-      rotate: '5deg',
-      duration: 250,
-      ease: 'outBack'
-    }, 250)
-    // website img flies away
-    .add('#brrl-website-img', {
-      left: '180%',
-      top: '30rem',
-      rotate: '20deg',
-      duration: 100,
-      ease: 'inBack'
-    }, 900)
+    if(window.matchMedia('(orientation: landscape)').matches) {
+      const tl = createTimeline({
+        defaults: {},
+      })
+      // menu img starting position
+      .add('#brrl-menu-img', {
+        right: [
+          {to: '-25%'}
+        ],
+        rotate: [
+          {to: '-40deg'}
+        ],
+        top: [
+          {to: '-25rem'}
+        ],
+        duration: 0
+      }, 0)
+      // menu img slides in from right
+      .add('#brrl-menu-img', {
+        right: [
+          {to: '19%' }
+        ],
+        rotate: [
+          {to:'8deg'}
+        ],
+        top: [
+          {to: '-10rem'}
+        ],
+        duration: 500
+      }, 250)
+      // menu img flies away to the right
+      .add('#brrl-menu-img', {
+        right: [
+          {to: '-25%'}
+        ],
+        rotate: [
+          {to: '-40deg'}
+        ],
+        top: [
+          {to: '-25rem'}
+        ],
+        duration: 150,
+        ease: 'inBack'
+      }, 750)
+      // text fades in
+      .add('#brrl-text', {
+        opacity: [0, 1],
+        translateY: ['-5rem', '0rem'],
+        duration: 250
+      }, 250)
+      // mobile img initial position
+      .add('#brrl-mobile-img', {
+        right: '-95%',
+        top: '50rem',
+        rotate: '20deg',
+        duration: 0
+      }, 0)
+      // mobile img flies in from the right
+      .add('#brrl-mobile-img', {
+        right: '25%',
+        top: '-15rem',
+        rotate: '-5deg',
+        duration: 500,
+        ease: 'outBack'
+      }, 50)
+      // mobile image moves down and rotates slightly to accompany menu-img
+      .add('#brrl-mobile-img', {
+        right: '22%',
+        rotate: '-2deg',
+        top: '-12rem',
+        duration: 250,
+        ease: 'outBack'
+      }, 250)
+      // mobile img flies away to the right
+      .add('#brrl-mobile-img', {
+        right: '55%',
+        top: '-125rem',
+        rotate: '20deg',
+        duration: 250,
+        ease: 'inBack'
+      }, 750)
+      // website img starting position
+      .add('#brrl-website-img', {
+        left: '-95%',
+        top: '30rem',
+        rotate: '20deg',
+        duration: 0
+      }, 0)
+      // website img slides in from the left
+      .add('#brrl-website-img', {
+        left: '15%',
+        top: '2rem',
+        rotate: '0deg',
+        duration: 250,
+        ease: 'inOutBack'
+      }, 1)
+      // website img move down and rotate slightly to accompany the text
+      .add ('#brrl-website-img', {
+        top: '14rem',
+        rotate: '5deg',
+        duration: 250,
+        ease: 'outBack'
+      }, 250)
+      // website img flies away
+      .add('#brrl-website-img', {
+        left: '180%',
+        top: '30rem',
+        rotate: '20deg',
+        duration: 100,
+        ease: 'inBack'
+      }, 900)
 
-    onScroll({
+      onScroll({
         target: '#brrl-showcase',
         axis: 'y',
         enter: '50% 0%',
         leave: '100% 100%',
         sync: true
-    }).link(tl);
+      }).link(tl);
+    } else {
+      const tl = createTimeline({
+        defaults: {},
+      })
+      // mobile img starting position
+      .add('#brrl-mobile-img', {
+        left: '-35%',
+        duration: 0
+      }, 0)
+      // mobile img flies in from the left
+      .add('#brrl-mobile-img', {
+        left: '10%',
+        top: '3rem',
+        duration: 500,
+        ease: 'outBack'
+      }, 1)
+      // mobile img slides to left to accomodate text
+      .add('#brrl-mobile-img', {
+        left: '-15%',
+        duration: 200
+      }, 500)
+      // mobile img flies away to the left
+      .add('#brrl-mobile-img', {
+        left: '135%',
+        top: '-20rem',
+        duration: 200,
+        ease: 'inBack'
+      }, 700)
+      // menu img initial position
+      .add('#brrl-menu-img', {
+        right: '-35%',
+        duration: 0
+      }, 0)
+      // menu img flies in from the right
+      .add('#brrl-menu-img', {
+        right: '10%',
+        top: '3rem',
+        duration: 500,
+        ease: 'outBack' 
+      }, 1)
+      // menu img slides to the right to accomodate text
+      .add ('#brrl-menu-img', {
+        right: '-15%',
+        duration: 200
+      }, 500)
+      // menu img flies away to the left
+      .add('#brrl-menu-img', {
+        right: '135%',
+        top: '20rem',
+        duration: 200,
+        ease: 'inBack'
+      }, 750)
+      // website img starting position
+      .add('#brrl-website-img', {
+        left: '-35%',
+        duration: 0
+      }, 0)
+      // website img slides in from the left
+      .add('#brrl-website-img', {
+        left: '0%',
+        top: '23rem',
+        duration: 500,
+        ease: 'outBack'
+      }, 1)
+      // website img floats around for a bit
+      .add('#brrl-website-img', {
+        left: '2%',
+        top: '24rem',
+        duration: 250,
+        ease: 'inOutBack'
+      }, 500)
+      // website img flies away to the bottom right
+      .add('#brrl-website-img', {
+        left: '135%',
+        top: '20rem',
+        duration: 200,
+        ease: 'inBack'
+      }, 750)
+      // text initial position
+      .add('#brrl-text', {
+        opacity: 0,
+        translateY: ['-5rem', '0rem'],
+        duration: 0
+      }, 0)
+      // text fades in
+      .add('#brrl-text', {
+        opacity: [0, 1],
+        translateY: ['-5rem', '0rem'],
+        duration: 250,
+        ease: 'outBack'
+      }, 500)
 
-    
+
+      onScroll({
+        target: '#brrl-showcase',
+        axis: 'y',
+        enter: '50% 0%',
+        leave: '100% 100%',
+        sync: true
+      }).link(tl);
+    }
   }
 
   // used on click to enlarge image and move to middle of screen
@@ -320,20 +433,22 @@ export class DevelopmentComponent implements AfterViewInit {
 
   // takes html identifier of element and adds looping 'floaty' animation
   addFloatAnimationToElement(element: string) {
-    animate(element, {
-      translateY: [
-      { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` },
-      { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` }
-      ],
-      translateX: [
-      { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` },
-      { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` }
-      ],
-      duration: Math.floor(Math.random() * (7000 - 4000 + 1)) + 4000,
-      ease: 'inOutSine',
-      loop: true,
-      alternate: true,
-      composition: 'blend'
-    });
+    if(window.matchMedia('(orientation: landscape)').matches) {
+      animate(element, {
+        translateY: [
+        { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` },
+        { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` }
+        ],
+        translateX: [
+        { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` },
+        { to: `${(Math.random() * 4 - 2).toFixed(2)}rem` }
+        ],
+        duration: Math.floor(Math.random() * (7000 - 4000 + 1)) + 4000,
+        ease: 'inOutSine',
+        loop: true,
+        alternate: true,
+        composition: 'blend'
+      });
+    }
   }
 }
